07577c53f4a4eba496aa43d7341e7f344ec4b441
rabindranath ferreira villamizar <rabindranathucv@gmail.com>
2020-01-03 00:02:38 -0300

    setup for test library

--- 02-javascript/index.js
+++ 02-javascript/index.js
@@ -9,7 +9,7 @@ Take a look at the 'requester' function.
 - What it does?
 ANSWER --> realiza una peticion http hacia una URI en especifico para consumir un endpoint donde retorna la respuesta en formato json. Implementacion basada en promesas.
 - How it's used? Add different use-case examples that covers every functionality.
-ANSWER -->  apiBaseUrl como referencia a locahost:3000/v1/, endpoint como referencia a algun recurso REST de dicha API
+ANSWER -->  apiBaseUrl como referencia a https://locahost:3000/v1/, endpoint como referencia a algun recurso REST de dicha API
 1.- requester('GET', 'apiBaseUrl/endpoint');
 2.- requester('POST', 'apiBaseUrl/endpoint', { 'Access-Control-Allow-Credentials': true, 'Access-Control-Allow-Origin', '*' });
 3.- requester('PUT', 'apiBaseUrl/endpoint/:id', { 'Access-Control-Allow-Credentials': true, 'Access-Control-Allow-Origin', '*' });
@@ -22,9 +22,12 @@ HINT: Use https://api.github.com/users/mediastream
 `);
 
 // Add fetch polyfill for Node.js
+require('es6-promise').polyfill();
 require('isomorphic-fetch'); // See: https://github.com/matthew-andrews/isomorphic-fetch
 
 function requester(method, base, headers = { Accept: '*/*' }) {
     return (path = []) => fetch((base ? [base, ...path] : path).join('/'), { method, headers })
         .then(r => r.json());
 }
+
+console.log(requester('GET', 'https://api.github.com/users/mediastream'));
