dbd90e6abecf9259920a1db7a51082dc2f949a39
rabindranath ferreira villamizar <rabindranathucv@gmail.com>
2020-01-03 02:52:23 -0300

    add frontend app

---
+++ 04-react/mediastream-app/src/app/components/list/list.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list-item/list-item.component';
+
+@NgModule({
+  declarations: [ListComponent, ListItemComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [ListComponent, ListItemComponent]
+})
+export class ListModule { }
