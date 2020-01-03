797d0ea576e9885923cb992eab0fa822c2fd0a33
rabindranath ferreira villamizar <rabindranathucv@gmail.com>
2020-01-03 03:11:55 -0300

    use nest components for list and item list, via input and validation for listdates

--- 04-react/mediastream-app/src/app/components/list/list-item/list-item.component.ts
+++ 04-react/mediastream-app/src/app/components/list/list-item/list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list-item',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListItemComponent implements OnInit {
 
+  @Input() date: any;
+  @Input() userId: any;
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.date);
+    console.log(this.userId);
   }
 
 }
